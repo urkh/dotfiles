79e5e5e136f8ac246ea26e157064d5051c5a3b42
Gustavo León Tramontin <gleontra@gmail.com>
2024-05-14 22:53:44 -0300

    Create plugins.lua

---
+++ plugins.lua
@@ -0,0 +1,89 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
+-- Install your plugins here
+return packer.startup(function(use)
+	use ("wbthomason/packer.nvim") -- Have packer manage itself	
+    use 'rktjmp/lush.nvim'
+    use 'nvim-tree/nvim-tree.lua'
+    use 'Vimjas/vim-python-pep8-indent'
+    use 'dense-analysis/ale'
+
+    use 'nvim-tree/nvim-web-devicons'
+    use 'joshdick/onedark.vim'
+    use 'uloco/bluloco.nvim'
+    use 'nvim-lualine/lualine.nvim'
+    use 'akinsho/bufferline.nvim'
+    use 'bluz71/vim-nightfly-colors'
+    use 'bluz71/vim-moonfly-colors'
+    use { "ibhagwan/fzf-lua",
+      requires = { "nvim-tree/nvim-web-devicons" }
+    }
+    use 'LunarVim/lunar.nvim'
+    use 'lewis6991/gitsigns.nvim'
+    use 'voldikss/vim-floaterm'
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use { 'rose-pine/neovim', as = 'rose-pine'}
+    use { 'd00h/nvim-rusticated', as = 'rusticated' }
+
+    use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x',
+      requires = {
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
+
+        {'neovim/nvim-lspconfig'},
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/cmp-nvim-lua'},
+        {'L3MON4D3/LuaSnip'},
+        {'rafamadriz/friendly-snippets'},
+      }
+    }
+
+
+
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
+end)
