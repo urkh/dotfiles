1b2cbe4c0c6d09c1c91d59778ab610be537307dc
Gustavo León Tramontin <gleontra@gmail.com>
2025-06-09 09:26:02 -0300

    Update plugins.lua

--- plugins.lua
+++ plugins.lua
@@ -1,82 +1,134 @@
-local fn = vim.fn
+ local fn = vim.fn
 
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+    PACKER_BOOTSTRAP =
+        fn.system(
+        {
+            "git",
+            "clone",
+            "--depth",
+            "1",
+            "https://github.com/wbthomason/packer.nvim",
+            install_path
+        }
+    )
+    print("Installing packer close and reopen Neovim...")
+    vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
+vim.cmd(
+    [[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]])
+]]
+)
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+    return
 end
 
 -- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
+packer.init(
+    {
+        display = {
+            open_fn = function()
+                return require("packer.util").float({border = "rounded"})
+            end
+        }
+    }
+)
 
 -- Install your plugins here
-return packer.startup(function(use)
-	use ("wbthomason/packer.nvim") -- Have packer manage itself	
-    use 'rktjmp/lush.nvim'  -- theme
-    use 'catppuccin/nvim'
-    use 'nvim-tree/nvim-tree.lua'
+return packer.startup(
+    function(use)
+        use("wbthomason/packer.nvim")
+        use "sainnhe/everforest"
+        use "Vimjas/vim-python-pep8-indent"
+        use "dense-analysis/ale"
 
-    use 'Vimjas/vim-python-pep8-indent'
-    use 'dense-analysis/ale'
+        use "nvim-tree/nvim-web-devicons"
+        use "akinsho/bufferline.nvim"
+        use {
+            "ibhagwan/fzf-lua",
+            requires = {"nvim-tree/nvim-web-devicons"}
+        }
 
-    use 'nvim-tree/nvim-web-devicons'
-    use 'nvim-lualine/lualine.nvim'
-    use 'akinsho/bufferline.nvim'
-    use { "ibhagwan/fzf-lua",
-      requires = { "nvim-tree/nvim-web-devicons" }
-    }
+        use "lewis6991/gitsigns.nvim"
+        use "voldikss/vim-floaterm"
 
-    use 'lewis6991/gitsigns.nvim'
-    use 'voldikss/vim-floaterm'
+        use(
+            {
+                "nvim-neo-tree/neo-tree.nvim",
+                branch = "v3.x",
+                requires = {
+                    "nvim-lua/plenary.nvim",
+                    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+                    "MunifTanjim/nui.nvim",
+                    "3rd/image.nvim",
+                }
+            }
+        )
 
-    use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v3.x',
-      requires = {
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
+        use {
+            "rmagatti/auto-session",
+            config = function()
+                require("auto-session").setup(
+                    {
+                        suppressed_dirs = {"~/", "~/Projects", "~/Downloads", "/"}
+                    }
+                )
+            end
+        }
 
-        {'neovim/nvim-lspconfig'},
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
-      }
-    }
-    
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
-end)
+        use {
+            "rmagatti/goto-preview"
+            -- requires = { 'rmagatti/logger.nvim' },
+        }
+
+        use {
+            "akinsho/git-conflict.nvim",
+            tag = "*",
+            config = function()
+                require("git-conflict").setup()
+            end
+        }
+
+        use "lukas-reineke/indent-blankline.nvim"
+        use "echasnovski/mini.indentscope"
+
+        -- lsp
+        use {
+            "neovim/nvim-lspconfig",
+            requires = {
+                {"mason-org/mason.nvim"},
+                {"mason-org/mason-lspconfig.nvim"}
+            }
+        }
+
+        -- auto complete
+        use {
+            "hrsh7th/nvim-cmp",
+            requires = {
+                "hrsh7th/cmp-nvim-lsp", -- Fuente: LSP
+                "L3MON4D3/LuaSnip", -- Snippets
+                "saadparwaiz1/cmp_luasnip" -- cmp + LuaSnip
+            }
+        }
+        use "yorickpeterse/vim-paper"
+        use {
+            "nvim-lualine/lualine.nvim",
+            requires = {"nvim-tree/nvim-web-devicons", opt = true}
+        }
+
+        if PACKER_BOOTSTRAP then
+            require("packer").sync()
+        end
+    end
+)
